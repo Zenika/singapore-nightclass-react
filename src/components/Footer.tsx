8635af3d1776f0a889febe4a4f2fbb2140bf202c
Thomas Huchedé <huchede.thomas@gmail.com>
2023-05-08 11:25:10 +0800

    style: update header/footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className={"flex justify-center bg-slate-900 p-8 text-white"}>
-      <p>2023 - Zenika</p>
+      <p>© 2023 - Zenika</p>
     </footer>
   );
 }
