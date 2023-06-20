c2fc76f3081faea93302f6be501685074fb4f30f
Thomas Huchedé <huchede.thomas@gmail.com>
2023-06-20 17:06:10 +0800

    feat: bypass html native validation

--- src/pages/cart/checkout.tsx
+++ src/pages/cart/checkout.tsx
@@ -75,7 +75,7 @@ export default function Checkout() {
     <div className="p-4 md:p-10">
       <h1 className="mb-4 text-5xl font-extrabold">Checkout</h1>
 
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} noValidate>
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
             Personal Information
