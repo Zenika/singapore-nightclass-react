dc6cd682ac286943d8f798ca77f4d652e358389a
Thomas Huchedé <huchede.thomas@gmail.com>
2023-06-20 17:31:14 +0800

    add: zod parsing on backend

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,15 +1,17 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import crypto from "crypto";
+import { orderSchema } from "~/models/order";
+import { z } from "zod";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-argument
-    const body: unknown = JSON.parse(req.body);
-    if (!body) {
-      res.status(400).send({ message: "missing body" });
+    const safelyParsedBody = orderSchema.safeParse(JSON.parse(req.body));
+    if (!safelyParsedBody.success) {
+      res.status(400).send(safelyParsedBody.error.errors);
     } else {
       const uuid = crypto.randomUUID();
-      const order = { ...body, uuid };
+      const order = { ...safelyParsedBody, uuid };
       console.log(`{NEW ORDER}[${uuid}] => ${JSON.stringify(order)}`);
       res.status(201).json({ orderId: uuid });
     }
